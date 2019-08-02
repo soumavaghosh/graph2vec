d7710b3c0b42fa6713aaeee5a9213869bdc1606c
Soumava Ghosh <ghosh.soumava4@gmail.com>
2019-08-02 19:27:15 +0530

    Add files via upload

--- text_graph.py
+++ text_graph.py
@@ -9,8 +9,15 @@ stop_words = list(set(stopwords.words('english')))
 rem = [',', '.', '?', ':', '...', '-', '"', "'", '!', "'s", "'nt", "'m", "'ve", "'d", '``', '\'\'', "'re", '(', ')', 'n\'t']
 stop_words.extend(rem)
 
+with open('../amazonreviews/train.ft.txt', 'r', encoding='utf-8') as f:
+    data = f.readlines()
+
 with open('../amazonreviews/test.ft.txt', 'r', encoding='utf-8') as f:
-    data = f.readlines()[:100]
+    data.extend(f.readlines())
+
+data = data[:10000]
+
+print('data read')
 
 label = ['0' if x.startswith('__label__1 ') else '1' for x in data]
 data = [x[11:] for x in data]
@@ -18,7 +25,11 @@ data = [x[11:] for x in data]
 with open('../amazonreviews/amazon_graph_indicator.txt', 'w') as f:
     f.write('\n'.join(label))
 
+print('started cleaning')
+print(len(data))
 for i in range(len(data)):
+    if (i+1)%1000 == 0:
+        print(i+1)
     if 'www.' in data[i] or 'http:' in data[i] or 'https:' in data[i] or '.com' in data[i]:
         data[i] = re.sub(r"([^ ]+(?<=\.[a-z]{3}))", "<url>", data[i])
     data[i] = sent_tokenize(data[i])
@@ -26,19 +37,34 @@ for i in range(len(data)):
     for j in range(len(data[i])):
         data[i][j] = [x for x in data[i][j] if x not in stop_words]
 
+print('data cleaned')
+
 vocab = []
-for d in data:
+for i in range(len(data)):
+    if (i+1)%1000 == 0:
+        print(i+1)
+    d = data[i]
     for s in d:
-        vocab.extend(s)
+        vocab.extend(list(set(s)))
 
-vocab = dict(Counter(vocab))
+vocab = list(set(vocab))
 
-word_to_id = {list(vocab.keys())[i]:i for i in range(len(vocab))}
-id_to_word = {i:list(vocab.keys())[i] for i in range(len(vocab))}
+word_to_id = {}
+id_to_word = {}
+
+print('creating map')
+
+for i, wrd in enumerate(vocab):
+    if (i + 1) % 1000 == 0:
+        print(i + 1)
+    word_to_id[wrd] = i
+    id_to_word[i] = wrd
 
 graph_id = {}
 window = 4
 ct = 1
+
+print('creating graphs')
 
 for d in data:
     g = defaultdict(list)
@@ -67,8 +93,13 @@ for d in data:
     graph_id[ct] = gph
     ct+=1
 
+print('graphs created')
+
+with open('amazon_graphs.json', 'wb') as f:
+    pickle.dump(graph_id, f)
 
 def get_encoding(g):
+
     enc = []
     enc_str = ''
     for i in list(g.keys()):
@@ -88,6 +119,8 @@ def get_encoding(g):
 graph_id_voc = {}
 
 for g in list(graph_id.keys()):
+    if (g + 1) % 1000 == 0:
+        print(g + 1)
     lst = []
     for n in list(graph_id[g].graph.keys()):
         if len(graph_id[g].graph[n])==0:
@@ -95,13 +128,14 @@ for g in list(graph_id.keys()):
         l = []
         graph_id[g].graph[n] = sorted(graph_id[g].graph[n])
         for d in range(1, 4):
-            #print(str(g)+' - '+str(n)+' - '+str(d))
             sub = graph_id[g].getsub(n,d,len(word_to_id))
             sub_enc = get_encoding(sub)
             l.append(sub_enc)
         l = list(set(l))
         lst.extend(l)
     graph_id_voc[g] = lst
+
+print('graph encoding done')
 
 with open('amazon_graph_voc_3.json', 'wb') as f:
     pickle.dump(graph_id_voc, f)
